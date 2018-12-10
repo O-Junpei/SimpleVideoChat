b121e014d728bb481534b2ff2056327e6058d4ce
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2018-12-11 00:40:58 +0900

    first commit

---
+++ Example/Pods/Target Support Files/Pods-SimpleVideoChat_Example/Pods-SimpleVideoChat_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleVideoChat_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleVideoChat_Example
+@end
---
+++ Example/Pods/Target Support Files/SimpleVideoChat/SimpleVideoChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SimpleVideoChat : NSObject
+@end
+@implementation PodsDummy_SimpleVideoChat
+@end
